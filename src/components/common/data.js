85dd2bea6f9c6d2c1633e197bd8b4fbf77a25b3d
ajithkanna1234 <ajithkanna78@gmail.com>
2025-06-05 22:42:14 +0530

    changes

--- src/components/common/data.js
+++ src/components/common/data.js
@@ -4,13 +4,14 @@ import Contact from "@/components/contact/Contact";
 import Nav from "@/components/nav/Nav";
 import { createRef } from "react";
 import { downloadResume } from "./downloadResume";
+import { ContactIcon, ContactRoundIcon } from "lucide-react";
 
 export const tabs = [
-  { id: 0,element:<Nav/> , className:"fixed top-2 w-4/5 "},
-  { id: 1, route: "home" ,element:<Home/>},
-  { id: 2, route: "project",element:<Project/> },
-  { id: 3, route: "contact",element:<Contact/> },
-  { id: 4, route: "resume", action: downloadResume },
+  { id: 0,icon:"",element:<Nav/> , className:"fixed top-2 w-4/5 "},
+  { id: 1,icon:"", route: "home" ,element:<Home/>},
+  { id: 2,icon:"", route: "project",element:<Project/> },
+  { id: 3,icon:<ContactRoundIcon className="bg-white text-black z-50"/>, route: "contact",element:<Contact/> },
+  { id: 4,icon:"", route: "resume", action: downloadResume },
 ];
 
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
 import { tabs } from "@/components/common/data";
+import GooeyNav from "@/components/magicui/nav-gooey";
 
 const Nav = ({ className }) => {
   const [activeTab, setActiveTab] = useState(tabs[0].id);
@@ -11,35 +12,47 @@ const Nav = ({ className }) => {
   };
 
   return (
-    <nav
-      className={`
-        ${className} 
-        flex items-center space-x-3 
-        lg:text-xl text-sm text-white bg-black/30 font-roboto font-light backdrop-blur-sm
-      `}
-    >
-      <div className="mr-auto size-10 border p-0.5">logo</div>
-      {tabs.map((tab) => (
-        <button
-          key={tab.id}
-          onClick={() => handleTabClick(tab)}
-          className={`
-            relative rounded px-3 py-1.5 flex items-center transition 
-            ${tab.id === 4 ? "!bg-slate-900" : ""}
-          `}
-          style={{ WebkitTapHighlightColor: "transparent" }}
-        >
-          {activeTab === tab.id && (
-            <motion.span
-              layoutId="bubble"
-              className="absolute inset-0 z-10 mix-blend-difference pl-0.5 flex items-center"
-              transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
-            />
-          )}
-          <span className="z-20 uppercase pb-0.5">{tab.route}</span>
-        </button>
-      ))}
-    </nav>
+<div style={{ height: '600px', position: 'relative' }}>
+  <GooeyNav
+    items={tabs}
+    particleCount={15}
+    particleDistances={[90, 10]}
+    particleR={100}
+    initialActiveIndex={0}
+    animationTime={600}
+    timeVariance={300}
+    colors={[1, 2, 3, 1, 2, 3, 1, 4]}
+  />
+</div>
+    // <nav
+    //   className={`
+    //     ${className} 
+    //     flex items-center space-x-3 
+    //     lg:text-xl text-sm text-white bg-black/30 font-roboto font-light backdrop-blur-sm
+    //   `}
+    // >
+    //   <div className="mr-auto size-10 border p-0.5">logo</div>
+    //   {tabs.map((tab) => (
+    //     <button
+    //       key={tab.id}
+    //       onClick={() => handleTabClick(tab)}
+    //       className={`
+    //         relative rounded px-3 py-1.5 flex items-center transition 
+    //         ${tab.id === 4 ? "!bg-slate-900" : ""}
+    //       `}
+    //       style={{ WebkitTapHighlightColor: "transparent" }}
+    //     >
+    //       {activeTab === tab.id && (
+    //         <motion.span
+    //           layoutId="bubble"
+    //           className="absolute inset-0 z-10 mix-blend-difference pl-0.5 flex items-center"
+    //           transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+    //         />
+    //       )}
+    //       <span className="z-20 uppercase pb-0.5">{tab.route}</span>
+    //     </button>
+    //   ))}
+    // </nav>
   );
 };
 
