5d72a9f76974599f21e0a68fc87c8d5487085869
Ajith kanna T <ajithkanna78@gmail.com>
2025-06-06 16:53:48 +0530

    feat: Add TypeScript support and refactor navigation component to use framer-motion for animations

--- src/components/common/data.js
+++ src/components/common/data.js
@@ -4,13 +4,13 @@ import Contact from "@/components/contact/Contact";
 import Nav from "@/components/nav/Nav";
 import { createRef } from "react";
 import { downloadResume } from "./downloadResume";
-import { ContactIcon, ContactRoundIcon } from "lucide-react";
+import { ContactIcon, ContactRoundIcon, House } from "lucide-react";
 
 export const tabs = [
   { id: 0,icon:"",element:<Nav/> , className:"fixed top-2 w-4/5 "},
-  { id: 1,icon:"", route: "home" ,element:<Home/>},
+  { id: 1,icon:<House/>, route: "home" ,element:<Home/>},
   { id: 2,icon:"", route: "project",element:<Project/> },
-  { id: 3,icon:<ContactRoundIcon className="bg-white text-black z-50"/>, route: "contact",element:<Contact/> },
+  { id: 3,icon:<ContactRoundIcon/>, route: "contact",element:<Contact/> },
   { id: 4,icon:"", route: "resume", action: downloadResume },
 ];
 
