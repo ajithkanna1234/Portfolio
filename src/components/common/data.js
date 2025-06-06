1fc70e514d0b7ba8c21c1626c0057d7d037ce66f
Ajith kanna T <ajithkanna78@gmail.com>
2025-06-06 17:01:26 +0530

    feat: Update tab icons in data.js for improved consistency and clarity

--- src/components/common/data.js
+++ src/components/common/data.js
@@ -4,14 +4,14 @@ import Contact from "@/components/contact/Contact";
 import Nav from "@/components/nav/Nav";
 import { createRef } from "react";
 import { downloadResume } from "./downloadResume";
-import { ContactIcon, ContactRoundIcon, House } from "lucide-react";
+import { ArrowDownToLine, CircleUserRound, FolderKanban, House } from "lucide-react";
 
 export const tabs = [
   { id: 0,icon:"",element:<Nav/> , className:"fixed top-2 w-4/5 "},
   { id: 1,icon:<House/>, route: "home" ,element:<Home/>},
-  { id: 2,icon:"", route: "project",element:<Project/> },
-  { id: 3,icon:<ContactRoundIcon/>, route: "contact",element:<Contact/> },
-  { id: 4,icon:"", route: "resume", action: downloadResume },
+  { id: 2,icon:<FolderKanban />, route: "project",element:<Project/> },
+  { id: 3,icon:<CircleUserRound/>, route: "contact",element:<Contact/> },
+  { id: 4,icon:<ArrowDownToLine />, route: "resume", action: downloadResume },
 ];
 
 
