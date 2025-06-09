dc18597dd7bed7282e2de3f10ad58b0f1d33bddd
ajithkanna1234 <ajithkanna78@gmail.com>
2025-06-09 21:28:19 +0530

    resume button

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,16 +6,19 @@ import profile from "@/components/assets/image.jpg"
 import Image from "next/image"
 import Orb from "@/components/animation/orb/Orb"
 import { DownloadCloudIcon } from "lucide-react"
+import { useResumeDownload } from "@/components/common/downloadResume"
 
 const Profile = () => {
+    const { downloadResume, downloadProgress, isDownloading } = useResumeDownload();
+
     const [view, setView] = useState(false)
-    useEffect(()=>{
+    useEffect(() => {
         setTimeout(() => {
             setView(true)
         }, 1000)
-    },[])
+    }, [])
     return (
-        <div className="md:text-8xl text-5xl lg:text-9xl font-bold p-[4%] relative z-10 tracking-widest grid gap-4">
+        <div className="md:text-8xl text-5xl lg:text-9xl font-bold p-[4%] relative z-10 tracking-widest h-full grid gap-4">
             <div className="space-y-4 mt-10 bg-transparent backdrop-blur-sm">
                 <DecryptedText text="DEVELOPER" className="text-white italic relative z-50" encryptedClassName="text-pink-700" />
                 <AnimatePresence initial={0}>
@@ -24,9 +27,9 @@ const Profile = () => {
                     </div>
                 </AnimatePresence>
             </div>
-            <div className={`flex flex-col justify-center h-fit bg-black text-sm md:text-base md:p-8 p-4 font-light font-roboto text-justify sm:w-1/2 tracking-wide text-white z-50 relative transition-all duration-700 ${view ? "scale-100" : "scale-0"}`}>
+            <div className={`flex flex-col justify-between h-full bg-black text-sm md:text-base md:p-8 p-4 font-light font-roboto text-justify sm:w-1/2 tracking-wide text-white z-50 relative transition-all duration-700 ${view ? "scale-100" : "scale-0"}`}>
                 MERN Stack Developer with 2 years of experience building scalable web applications using React.js, Node.js, Express, and MongoDB. and collaborating in Agile teams. Skilled in UI/UX design, RESTful APIs, and state management ( Redux Toolkit, Context API). Certified Full-Stack Developer with a focus on clean code and responsive design.
-                {/* <Image src={pikachu} alt="pikachu" className="h-10 w-fit" /> */}
+                <button className="cursor-pointer size-fit font-medium px-3 py-2 mx-auto border transition-all duration-100 ease-linear rounded-full hover:bg-white hover:text-black" onClick={downloadResume}>{isDownloading ? <Image src={pikachu} alt="pikachu" className="h-5 w-fit" /> : "Resume"}</button>
             </div>
             <div className="h-screen w-full md:w-1/2 right-0 top-0 fixed z-0">
                 <Orb
